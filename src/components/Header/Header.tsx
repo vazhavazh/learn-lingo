95419871b0f3aa03d47a8bd12e037bbea1e8707e
vazhavazh <118344739+vazhavazh@users.noreply.github.com>
2023-08-17 10:52:06 +0300

    add favicon

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+type HeaderProps = {
+    
+};
+
+const Header:React.FC<HeaderProps> = () => {
+    
+    return <h1>header</h1>
+}
+export default Header;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,27 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	return (
+		<>
+			<Head>
+				<title>Learn Lingo</title>
+				<meta
+					name='description'
+					content='Web applications that help people to 
+          find the best teacher'
+				/>
+				<meta
+					name='viewport'
+					content='width=device-width, initial-scale=1'
+				/>
+				<link
+					rel='icon'
+					href='/favicon.svg'
+				/>
+			</Head>
+			<Component {...pageProps} />
+		</>
+	);
 }
