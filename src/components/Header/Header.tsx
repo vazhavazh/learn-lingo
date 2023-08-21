0684876602d613af0f000dc30e5e8fc4b5ffeb58
vazhavazh <118344739+vazhavazh@users.noreply.github.com>
2023-08-21 04:27:57 +0300

    upd

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,16 +1,22 @@
 import Image from 'next/legacy/image'
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 import React from 'react'
 
 type HeaderProps = {}
 
 const Header: React.FC<HeaderProps> = () => {
+    const router = useRouter()
+
     return (
         <header
             className="flex justify-between items-center py-3 sm:py-5 px-1 
 		sm:px-8 md:px-16  lg:px-32"
         >
-            <Link href="/" className="flex gap-2 items-center">
+            <Link
+                href="/"
+                className="flex gap-2 items-center transition-all hover:scale-125"
+            >
                 <Image src="/logo.svg" alt="logo" width={28} height={28} />
                 <span
                     className="text-gray-900 text-[8px] sm:text-xl 
@@ -24,14 +30,26 @@ const Header: React.FC<HeaderProps> = () => {
                 className=" flex gap-2 sm:gap-7 text-gray-900 text-[8px] sm:text-base 
 			not-italic font-normal leading-[125%]"
             >
-                <Link href="/">Home</Link>
-                <Link href="/teachers">Teachers</Link>
+                <Link
+                    className={`${router.pathname === '/' ? 'text-hero-btn' : ''}`}
+                    href="/"
+                >
+                    Home
+                </Link>
+                <Link
+                    className={`${
+                        router.pathname === '/teachers' ? 'text-hero-btn' : ''
+                    }`}
+                    href="/teachers"
+                >
+                    Teachers
+                </Link>
             </div>
 
             <div className="flex gap-2 sm:gap-4 items-center">
                 <button
                     className="flex gap-1 items-center sm:gap-2 text-gray-900 text-[8px] sm:text-base 
-				not-italic font-bold leading-[125%]"
+				not-italic font-bold leading-[125%] transition-all hover:scale-125"
                 >
                     <Image
                         src="/login.svg"
--- src/pages/teachers/index.tsx
+++ src/pages/teachers/index.tsx
@@ -1,8 +1,14 @@
-import React from "react";
+import Header from '@/components/Header/Header'
+import React from 'react'
 
-type teachersProps = {};
+type teachersProps = {}
 
 const TeachersPage: React.FC<teachersProps> = () => {
-	return <h1>TEachers Page</h1>;
-};
-export default TeachersPage;
+    return (
+        <>
+            <Header />
+            <div className="px-2 m680:px-16 lg:px-32 border border-solid border-red-500"></div>
+        </>
+    )
+}
+export default TeachersPage
